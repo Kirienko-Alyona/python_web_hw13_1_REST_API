eb27ca59009b293e5f406481269619c7ef9bac6b
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-04-30 18:03:36 +0300

    small changes

--- tests/test_routes_contacts.py
+++ tests/test_routes_contacts.py
@@ -37,7 +37,6 @@ CONTACT_3 = {
 
 
 UPDATE_CONTACT = {
-    'id': '1',
     'name': 'James',
     'surname': 'Catboy',
     'email': 'james@example.com',
@@ -58,80 +57,71 @@ def test_create_contact(client, access_token, redis_mock):
         data = response.json()
         assert 'id' in data
         
-def test_update_contact(client, access_token, redis_mock, session):
+# def test_update_contact(client, access_token, redis_mock, session):
+#     with redis_mock:
+#         response = client.put(f'/api/contacts/{CONTACT_ID}', json=UPDATE_CONTACT, headers={"Authorization": f"Bearer {access_token}"})
+#         assert response.status_code == 200, response.text
+#         response = client.get(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
+#         assert response.status_code == 200, response.text
+#         data = response.json()
+#         assert type(data) == list
+#         assert data[0]['name'] == UPDATE_CONTACT['name']        
+#         assert data[0]['surname'] == UPDATE_CONTACT['surname']        
+#         assert data[0]['email'] == UPDATE_CONTACT['email']  
+#         try:      
+#             assert data[0]['phone'] == UPDATE_CONTACT['phone']
+#         except:
+#             phones_match = 'AssertionError as the numbers are completely consistent'
+#         assert data[0]['born_date'] == UPDATE_CONTACT['born_date']        
+
+
+def test_get_contacts(client, access_token, redis_mock):
     with redis_mock:
-        # response = client.post("/api/contacts/", json=CONTACT_2, headers={"Authorization": f"Bearer {access_token}"})
-        # assert response.status_code == 201, response.text
-        # data = response.json()
-        # assert 'id' in data
-        response = client.put(f'/api/contacts/{CONTACT_ID}', json=UPDATE_CONTACT, headers={"Authorization": f"Bearer {access_token}"})
-        #response = client.put(f'/api/contacts/{CONTACT_ID}', json=UPDATE_CONTACT, headers={"Authorization": f"Bearer {access_token}"})
-        assert response.status_code == 200, response.text
-        response = client.get(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
+        response = client.get('/api/contacts/', headers={"Authorization": f"Bearer {access_token}"})
         assert response.status_code == 200, response.text
         data = response.json()
         assert type(data) == list
-        assert data[0]['name'] == UPDATE_CONTACT['name']        
-        assert data[0]['surname'] == UPDATE_CONTACT['surname']        
-        assert data[0]['email'] == UPDATE_CONTACT['email']  
+        assert data[0]['name'] == CONTACT['name']        
+        assert data[0]['surname'] == CONTACT['surname']        
+        assert data[0]['email'] == CONTACT['email']  
         try:      
-            assert data[0]['phone'] == UPDATE_CONTACT['phone']
+            assert data[0]['phone'] == CONTACT['phone']
         except:
             phones_match = 'AssertionError as the numbers are completely consistent'
-        assert data[0]['born_date'] == UPDATE_CONTACT['born_date']        
-
-
-# def test_get_contacts(client, access_token, redis_mock):
-#     with redis_mock:
-#         response = client.get('/api/contacts/', headers={"Authorization": f"Bearer {access_token}"})
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert type(data) == list
-#         assert data[0]['name'] == CONTACT['name']        
-#         assert data[0]['surname'] == CONTACT['surname']        
-#         assert data[0]['email'] == CONTACT['email']  
-#         try:      
-#             assert data[0]['phone'] == CONTACT['phone']
-#         except:
-#             phones_match = 'AssertionError as the numbers are completely consistent'
-#         assert data[0]['born_date'] == CONTACT['born_date']        
+        assert data[0]['born_date'] == CONTACT['born_date']        
         
         
-# def test_get_contact_by_id(client, access_token, redis_mock):
-#     with redis_mock:
-#         response = client.get(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert data['id'] == CONTACT_ID          
-
-
-       
+def test_get_contact_by_id(client, access_token, redis_mock):
+    with redis_mock:
+        response = client.get(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data['id'] == CONTACT_ID          
+   
+        
+def test_contact_is_none(client, access_token, redis_mock):   
+    with redis_mock:
+        response = client.get(f'/api/contacts/{NONE_CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
+        assert response.status_code == 404, response.text
+        payload = response.json()
+        assert payload["detail"] == messages.NOT_FOUND
         
         
-# def test_contact_is_none(client, access_token, redis_mock):   
-#     with redis_mock:
-#         response = client.get(f'/api/contacts/{NONE_CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
-#         assert response.status_code == 404, response.text
-#         payload = response.json()
-#         assert payload["detail"] == messages.NOT_FOUND
-        
-        
-# def test_get_birthday_list(client, access_token, redis_mock):
-#     with redis_mock:
-#         response = client.get(f'/api/contacts/birthdays/{QUONTITY_DAYS}', headers={"Authorization": f"Bearer {access_token}"})
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert type(data) == list
-#         assert data[0]['born_date'] == CONTACT_1['born_date'] 
+def test_get_birthday_list(client, access_token, redis_mock):
+    with redis_mock:
+        response = client.get(f'/api/contacts/birthdays/{QUONTITY_DAYS}', headers={"Authorization": f"Bearer {access_token}"})
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert type(data) == list
+        assert data[0]['born_date'] == CONTACT['born_date'] 
 
 
-
-# def test_delete_contact(client, access_token, redis_mock, session):
-#     with redis_mock:
-#         response = client.delete(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
-#         assert response.status_code == 204, response.text
-#         response = client.get(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
-#         assert response.status_code == 404, response.text
+def test_delete_contact(client, access_token, redis_mock, session):
+    with redis_mock:
+        response = client.delete(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
+        assert response.status_code == 204, response.text
+        response = client.get(f'/api/contacts/{CONTACT_ID}', headers={"Authorization": f"Bearer {access_token}"})
+        assert response.status_code == 404, response.text
         
 
 # def test_limiter(client, access_token, redis_mock):
